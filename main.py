9e3e9ce5c033cbfa19f3d7e1332ec087b09bc668
Hillary Chan <hillary.chan91@gmail.com>
2025-05-24 17:53:29 +1000

    lint: use Path.expanduser

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import shutil
 import subprocess
 from enum import Enum
 from os import path
+from pathlib import Path
 from typing import TYPE_CHECKING, Any
 
 from ulauncher.api.client.EventListener import EventListener
@@ -62,11 +63,11 @@ class FuzzyFinderExtension(Extension):
         errors = []
 
         base_dir = preferences["base_dir"]
-        if not path.isdir(path.expanduser(base_dir)):
+        if not path.isdir(Path(base_dir).expanduser()):
             errors.append(f"Base directory '{base_dir}' is not a directory.")
 
         ignore_file = preferences["ignore_file"]
-        if ignore_file and not path.isfile(path.expanduser(ignore_file)):
+        if ignore_file and not path.isfile(Path(ignore_file).expanduser()):
             errors.append(f"Ignore file '{ignore_file}' is not a file.")
 
         try:
@@ -94,8 +95,8 @@ class FuzzyFinderExtension(Extension):
             "follow_symlinks": bool(int(input_preferences["follow_symlinks"])),
             "trim_display_path": bool(int(input_preferences["trim_display_path"])),
             "result_limit": int(input_preferences["result_limit"]),
-            "base_dir": path.expanduser(input_preferences["base_dir"]),
-            "ignore_file": path.expanduser(input_preferences["ignore_file"]),
+            "base_dir": Path(input_preferences["base_dir"]).expanduser(),
+            "ignore_file": Path(input_preferences["ignore_file"]).expanduser(),
         }
 
         logger.debug("Using user preferences %s", preferences)
